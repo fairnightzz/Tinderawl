1c639ecfcfd70a52436a4db577dee489a04ee617
Daniel Liu <mr.picklepinosaur@gmail.com>
2020-05-26 01:12:57 -0400

    switched to ts

---
+++ web/src/app.ts
@@ -0,0 +1,5 @@
+function hello(a: string): string {
+    return `Hello, ${a}!`;
+}
+
+console.log(hello('Daniel Liu'));
--- web/src/lmaoapp.js
+++ web/src/lmaoapp.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import Accordion from './components/Accordion.vue';
 import List from './components/List.vue';
 
+window.axios = require('axios');
+
 new Vue({
     el: '#app',
 
@@ -18,7 +20,9 @@ new Vue({
             { title: "Item 1", description: "this is an item" },
             { title: "Item 2", description: "this is an item lmao" },
             { title: "Item 3", description: "this is an item xd" }
-        ]
+        ],
+
+        posts: null,
     },
 
     computed: {
@@ -28,6 +32,14 @@ new Vue({
 
     methods: {
         moneyFormat: function(price) { return `$${Math.round(price)}`}
-    }
+    },
+
+    mounted: function() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => this.posts = response)
+            .catch(error => this.posts = [{id: -1, title: 'No posts found...'}]) //for some reason 404 is not being caught
+            .finally(() => console.log("DONE"));
+
+    },
 
 });
