716ddffa4f37725a12699e9908fbe2ec6669ed77
Zhehai Zhang <zhehaizhang1@gmail.com>
2020-06-02 14:22:17 -0400

    dope

--- backend/setUp.js
+++ backend/setUp.js
@@ -1,5 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 //Set up db linking pics to file names
+const picpath = './Pictures/';
+const fs = require('fs');
 
 let db = new sqlite3.Database('./databases/database.db', function(error){
     if (error){
@@ -10,21 +12,45 @@ let db = new sqlite3.Database('./databases/database.db', function(error){
     }
 })
 
+
+
+fs.readdirSync("./Pictures/").forEach(file => {
+        
+    //If good: Generate code
+    const charac = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    var code = "";
+        for (var i = 0; i<10;i++){
+            code+=charac.charAt(Math.round(Math.random()*(charac.length-1)));
+        }
+        var filename = code+file.substring(file.indexOf("."),file.length)
+        fs.renameSync(picpath+file,picpath+filename,function(err){
+            if (err){
+                console.log(err);
+            }
+        })
+
+})
+
+
+
+
 db.run('CREATE TABLE IF NOT EXISTS pic(num int,name varchar(255))',function(error){
     if(error){
         console.log (error);
         return;
     }
-    const picpath = './Pictures/';
-    const fs = require('fs');
+    var count = 1;
+    
+    
     fs.readdirSync(picpath).forEach(file => {
-        console.log(file);
-        db.run('INSERT INTO pic(num,name) VALUES(?,?)',[parseInt(file.substring(0,file.indexOf("."))),file],function(err){
+        
+        db.run('INSERT INTO pic(num,name) VALUES(?,?)',[count,file],function(err){
             if (err){
                 return console.log(err.message);
             }
         });
         console.log("inserted")
+        count+=1;
 
     })
             
