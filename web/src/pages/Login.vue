9efb4c75e9f25340fb4ab695536492970a5eb264
Daniel Liu <mr.picklepinosaur@gmail.com>
2020-06-05 17:30:00 -0400

    a

--- web/src/pages/Login.vue
+++ web/src/pages/Login.vue
@@ -34,7 +34,7 @@
 
             <!-- v-text="'</> made with love'" -->
             <div class="row justify-content-md-center">
-                <a href="https://github.com/fairnightzz/Tinderawl" style="text-decoration-color: white">
+                <a href="https://github.com/fairnightzz/Tinderwal" style="text-decoration-color: white">
                     <p style="color: white; padding: .5rem; font-size: 12px" v-text="'</> happy birthday fadilah'"></p>
                 </a>
             </div>
